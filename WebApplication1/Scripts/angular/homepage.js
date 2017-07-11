882717c7327e73ae42ed7e46d3edc6115e3a0c4e
rocallaghan <rocallaghan@RUBICON-PC01>
2017-07-11 15:18:54 +0100

    Add project files.

---
+++ WebApplication1/Scripts/angular/homepage.js
@@ -0,0 +1,30 @@
+﻿// Define the 'storeDetailApp' 
+var kevinfoleyapp = angular.module('kevinfoleyApp', ['ui.bootstrap', 'ui.grid', 'ui.grid.pagination', 'ui.grid.resizeColumns', 'ngSanitize', 'ui.select', 'angular-advanced-searchbox', 'xeditable']);
+
+// Define the `PhoneListController` controller on the `phonecatApp` module
+kevinfoleyapp.controller('HomePageController', function HomePageController($scope, $http, $timeout, $window, $filter) {
+
+
+    $scope.AnimateKevsHead = function () {
+        var elem = document.getElementById("myAnimation");
+        var pos = 0;
+        var id = setInterval(frame, 10);
+        function frame() {
+            if (pos == 350) {
+                clearInterval(id);
+            } else {
+                pos++;
+                elem.style.top = pos + 'px';
+                elem.style.left = pos + 'px';
+            }
+        }
+    }
+
+
+
+    $scope.init = function () {
+        $scope.AnimateKevsHead();
+
+    }
+
+});
